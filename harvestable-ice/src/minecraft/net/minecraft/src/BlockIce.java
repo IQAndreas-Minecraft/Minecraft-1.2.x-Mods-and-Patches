1fbe73437dec287b3da43183a3f8b3c89b4bdfe7
Andreas Renberg (IQAndreas) <iq_andreas@hotmail.com>
2012-04-14 15:25:21 +0200

    [harvestable-ice] Source code and compile v1.2.5-01
    Changes made:
    * Ice blocks can be harvested with the Silk Touch enchantment
    * Ice blocks will randomly melt in the Nether
    * If an ice block melts or is broken in the Nether, it will fizzle and disappear instead of turning to water (similar to emptying a water bucket in the Nether)

--- harvestable-ice/src/minecraft/net/minecraft/src/BlockIce.java
+++ harvestable-ice/src/minecraft/net/minecraft/src/BlockIce.java
@@ -36,10 +36,10 @@ public class BlockIce extends BlockBreakable
     {
         super.harvestBlock(par1World, par2EntityPlayer, par3, par4, par5, par6);
         Material material = par1World.getBlockMaterial(par3, par4 - 1, par5);
-
+        
         if (material.blocksMovement() || material.isLiquid())
         {
-            par1World.setBlockWithNotify(par3, par4, par5, Block.waterMoving.blockID);
+        	this.convertToWater(par1World, par3, par4, par5);
         }
     }
 
@@ -56,12 +56,40 @@ public class BlockIce extends BlockBreakable
      */
     public void updateTick(World par1World, int par2, int par3, int par4, Random par5Random)
     {
-        if (par1World.getSavedLightValue(EnumSkyBlock.Block, par2, par3, par4) > 11 - Block.lightOpacity[blockID])
+        if ((par1World.worldProvider.isHellWorld) || (par1World.getSavedLightValue(EnumSkyBlock.Block, par2, par3, par4) > 11 - Block.lightOpacity[blockID]))
         {
+        	//Not sure why this is here or if it is needed. I'll just leave it.
             dropBlockAsItem(par1World, par2, par3, par4, par1World.getBlockMetadata(par2, par3, par4), 0);
-            par1World.setBlockWithNotify(par2, par3, par4, Block.waterStill.blockID);
+            
+            this.convertToWater(par1World, par2, par3, par4); 
         }
     }
+    
+
+    /**
+     * Destroys the water block and converts it to water (unless in the Nether)
+     */
+    public void convertToWater(World world, int x, int y, int z)
+    {
+    	if (world.worldProvider.isHellWorld)
+        {
+        	//Cannot create water in the nether, create air.
+    		world.setBlockWithNotify(x, y, z, 0);
+        	
+        	//Taken from 'ItemBucket::onItemRightClick()'
+    		world.playSoundEffect(x + 0.5D, y + 0.5D, z + 0.5D, "random.fizz", 0.5F, 2.6F + (world.rand.nextFloat() - world.rand.nextFloat()) * 0.8F);
+            for (int l = 0; l < 8; l++)
+            {
+            	world.spawnParticle("largesmoke", (double)x + Math.random(), (double)y + Math.random(), (double)z + Math.random(), 0.0D, 0.0D, 0.0D);
+            }
+        }
+        else
+        {
+        	//Melts to water
+        	world.setBlockWithNotify(x, y, z, Block.waterStill.blockID);
+        }
+    }
+
 
     /**
      * Returns the mobility information of the block, 0 = free, 1 = can't push but can move over, 2 = total immobility
@@ -78,6 +106,7 @@ public class BlockIce extends BlockBreakable
      */
     protected ItemStack createStackedBlock(int par1)
     {
-        return null;
+    	//	Can now be harvested with silk touch
+    	return super.createStackedBlock(par1);
     }
 }

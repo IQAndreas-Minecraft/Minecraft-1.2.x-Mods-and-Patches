cee485461a62a89c47eca3439907e7af614ad67b
Andreas Renberg (IQAndreas) <iq_andreas@hotmail.com>
2012-04-15 20:24:47 +0200

    [iqandreas-fixes] Moved double slab recipes to separate patch
    New patch is named "Craftable Double Slabs Patch v1.2.5-01"
    IQAndreas Fixes Patch is now v1.2.5-02

--- craftable-double-slabs/src/minecraft/net/minecraft/src/RecipesCrafting.java
+++ craftable-double-slabs/src/minecraft/net/minecraft/src/RecipesCrafting.java
@@ -31,10 +31,6 @@ public class RecipesCrafting
                 {
                     "##", "##", '#', Block.sandStone
                 });
-        par1CraftingManager.addRecipe(new ItemStack(Block.sandStone, 1, 1), new Object[]
-                {
-                    "#", "#", '#', new ItemStack(Block.stairSingle, 1, 1)
-                });
         par1CraftingManager.addRecipe(new ItemStack(Block.stoneBrick, 4), new Object[]
                 {
                     "##", "##", '#', Block.stone
@@ -50,6 +46,46 @@ public class RecipesCrafting
         par1CraftingManager.addRecipe(new ItemStack(Block.redstoneLampIdle, 1), new Object[]
                 {
                     " R ", "RGR", " R ", 'R', Item.redstone, 'G', Block.glowStone
+                });
+        
+        /* ---------- Double half-slab blocks -------
+        	0x0	 Stone Slab
+   	 		0x1	 Sandstone Slab
+   	 		0x2	 Wooden Slab
+   	 		0x3	 Cobblestone Slab
+   	 		0x4	 Brick Slab
+   	 		0x5	 Stone Brick Slab
+   	 	*/
+        
+        par1CraftingManager.addRecipe(new ItemStack(Block.stairDouble, 1, 0), new Object[]
+                { // Smooth stone double slabs
+                    "#", "#", '#', new ItemStack(Block.stairSingle, 1, 0)
+                });
+        par1CraftingManager.addRecipe(new ItemStack(Block.sandStone, 1, 1), new Object[]
+                { // Sandstone (creates special sandstone blocks)
+                    "#", "#", '#', new ItemStack(Block.stairSingle, 1, 1)
+                });
+        par1CraftingManager.addRecipe(new ItemStack(Block.stairDouble, 1, 2), new Object[]
+                { // Wooden slab (creates fireproof wood blocks)
+                    "#", "#", '#', new ItemStack(Block.stairSingle, 1, 2)
+                });
+        par1CraftingManager.addRecipe(new ItemStack(Block.stairDouble, 1, 3), new Object[]
+                {
+        			// Cobblestone block (not double slab!)
+                    //"#", "#", '#', new ItemStack(Block.stairSingle, 1, 3)
+        			"#", "#", '#', new ItemStack(Block.cobblestone)
+                });
+        par1CraftingManager.addRecipe(new ItemStack(Block.stairDouble, 1, 4), new Object[]
+                { 
+        			// Brick block (not double slab!)
+                    //"#", "#", '#', new ItemStack(Block.stairSingle, 1, 4)
+        			"#", "#", '#', new ItemStack(Block.brick)
+                });
+        par1CraftingManager.addRecipe(new ItemStack(Block.stairDouble, 1, 5), new Object[]
+                { 
+        			// Stone brick block (not double slab!)
+                    //"#", "#", '#', new ItemStack(Block.stairSingle, 1, 5)
+        			"#", "#", '#', new ItemStack(Block.stoneBrick)
                 });
     }
 }
